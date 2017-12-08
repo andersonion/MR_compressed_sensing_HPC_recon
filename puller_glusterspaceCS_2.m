43b3d20c28e9426c4712cdf0e4232dc16c97bf75
James Cook <jamesjcook@users.noreply.github.com>
2017-12-08 15:08:22 -0500

    Whitespace cleanup.

--- puller_glusterspaceCS_2.m
+++ puller_glusterspaceCS_2.m
@@ -3,7 +3,6 @@ function puller_glusterspaceCS_2(runno,datapath,scanner,workpath,mode,overwrite)
 % mode: 1 -> pull fid, 2 -> pull procpar, 3 -> pull both.
 % This function name is terrible, and only marginally connects to its
 % purpose. -James 20171204
-
 %check arguments
 if nargin<5
     error('not enough input arguments');
@@ -12,7 +11,6 @@ elseif nargin==5
 elseif nargin~=6
     error('too many input arguments arguments');
 end
-
 %create work directory and handle overwrite option
 if exist(workpath,'dir') && overwrite==1
     display('data already exists in workpath and you have specified to overwrite existing data');
@@ -21,20 +19,15 @@ if exist(workpath,'dir') && overwrite==1
 elseif ~exist(workpath,'dir')
     mkdir(workpath);
 end
-
 fid_pull_cmd=['scp omega@' scanner ':' datapath '/fid ' workpath '/' runno '.fid'];
 procpar_pull_cmd=['scp omega@' scanner ':' datapath '/procpar ' workpath '/' runno '.procpar'];
-
-
 %pull the fid file and the procpar to the work directory if they dont exist
 %if they do exist check filesize
 if (mode == 1) || (mode == 3)
     if ~exist([workpath '/' runno '.fid'],'file')
-        
         %system(fid_pull_cmd);
         status = 1;
         logged=0;
-        
         [status,~] = system(fid_pull_cmd);
         if status
             %error_flag=1;
@@ -43,10 +36,9 @@ if (mode == 1) || (mode == 3)
             %yet_another_logger(log_msg,log_mode,log_file,error_flag);
             error_due_to_network_issues
             %quit
-        end        
+        end
     end
 end
-
 if (mode == 2) || (mode == 3)
     if ~exist([workpath '/' runno '.procpar'],'file')
         %system(procpar_pull_cmd);
@@ -60,8 +52,6 @@ if (mode == 2) || (mode == 3)
             %yet_another_logger(log_msg,log_mode,log_file,error_flag);
             error_due_to_network_issues
             %quit
-        end     
+        end
     end
 end
-
-
