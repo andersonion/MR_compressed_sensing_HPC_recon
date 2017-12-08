b8175222cbc2b06c674894141b70f4f3c69aee42
James Cook <jamesjcook@users.noreply.github.com>
2017-12-08 10:43:39 -0500

    Swiched to ssh_call from system.

--- find_input_fidCS.m
+++ find_input_fidCS.m
@@ -1,43 +1,28 @@
 function [ input_fid, local_or_streaming_or_static ] = find_input_fidCS( scanner,runno,study,agilent_series,user )
 %
 % local_or_streaming_or_static: 1 => local fid found, 2 -> using fid of in-progress scan, 3 -> using fid in its remote static location;
-
 if ~exist('user','var')
     user='omega';
 end
-
 local_or_streaming_or_static = 3;
-
 scratch_drive = getenv('BIGGUS_DISKUS'); % Currently, this is /glusterspace.
 workdir = fullfile(scratch_drive,[runno '.work/']);
 local_fid = [workdir runno '.fid'];
-
 if exist(local_fid,'file')
     input_fid = local_fid;
     local_or_streaming_or_static = 1;
 else
     finished_acq_path=['/home/mrraw/' study '/' agilent_series '.fid/fid'];
     current_fid_size=get_remote_filesize(finished_acq_path,scanner);% check final destination
-    
     if (current_fid_size == 0)
         find_file_cmd=['ssh ' user '@' scanner ' "ls -tr /home/vnmr1/vnmrsys/exp*/acqfil/fid | tail -n1"'];
-        [status,result]=system(find_file_cmd);
-        if status
-            %error_flag=1;
-            log_msg=sprintf('Failure due to network connectivity issues; unsuccessful communication with %s.\n',scanner);
-            %yet_another_logger(log_msg,log_mode,log_file,error_flag);
-            disp(log_msg)
-            error_due_to_network_issues
-            %quit
-        end
+        [~,result]=ssh_call(find_file_cmd);
         latest_fid=result(1:end-1);
         input_fid=latest_fid;
         local_or_streaming_or_static = 2;
     else
         input_fid=finished_acq_path;
     end
-    
 end
-    
 end
 
