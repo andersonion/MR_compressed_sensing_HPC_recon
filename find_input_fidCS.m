8c9ad89e33a7f5c301d534c5e648ca80ea398e6e
James Cook <jamesjcook@users.noreply.github.com>
2017-12-08 15:02:32 -0500

    Reverted code, and left a couple examples.

--- find_input_fidCS.m
+++ find_input_fidCS.m
@@ -17,17 +17,26 @@ else
     if (current_fid_size == 0)
         % I think using tail in ssh call is generating bogus status codes.
         % Will approximate this behavior inside matlab by taking last item.
-        %{
         find_file_cmd=['ssh ' user '@' scanner ' "ls -tr /home/vnmr1/vnmrsys/exp*/acqfil/fid | tail -n1"'];
         [~,result]=ssh_call(find_file_cmd);
         latest_fid=result(1:end-1);
         input_fid=latest_fid;
         %}
+        %{
+        % This command continued to have trouble, going to try switching to
+        a find command. 
         inprogress_fid_cmd=['ssh ' user '@' scanner ' ls -tr /home/vnmr1/vnmrsys/exp*/acqfil/fid'];
         [~,inprogress_fids]=ssh_call(inprogress_fid_cmd);
         inprogress_fids=strsplit(inprogress_fids);
         input_fid=inprogress_fids{end-1};
         local_or_streaming_or_static = 2;
+        %}
+        %{
+        find_file_cmd=['ssh ' user '@' scanner ' "$(find  /home/vnmr1/vnmrsys/ -type d -maxdepth 1 -name \"exp*\") | tail -n1"'];
+        [~,result]=ssh_call(find_file_cmd);
+        latest_fid=result(1:end-1);
+        input_fid=latest_fid;
+        %}
     else
         input_fid=finished_acq_path;
     end
