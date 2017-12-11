8e2812280a147545fe7a530f452836d2865e6ad2
James Cook <jamesjcook@users.noreply.github.com>
2017-12-11 12:10:16 -0500

    Added wait for completion on parallel compile.

--- compile__all.m
+++ compile__all.m
@@ -1,7 +1,7 @@
 % compile all
 
 parallel=1;
-[~,c_commands]= system('find . -name "compile_command_*.m"|grep -v "__"')
+[~,c_commands]= system(sprintf('find %s -name "compile_command_*.m"|grep -v "__"',pwd))
 c_commands=strsplit(c_commands);
 if ~parallel
     %% serial run of compile commands in matlab
@@ -17,17 +17,28 @@ if ~parallel
     end
 else
     %% parallel run using shell parllelism, unfortunately doesnt wait for completion yet.
+    list_active_compiles_cmd=sprintf('ps -ef|grep -i matlab |grep %s |grep compile_command\n',getenv('USER'));
     warning('Background_compile starting! compile logs will be in /tmp/');
     for c=1:numel(c_commands)
+        [~,n]=fileparts(c_commands{c});
         if ~isempty(c_commands{c})
-            system(sprintf('matlab -nodisplay -nosplash -nodesktop -r "run %s;exit" -logfile /tmp/%s.log & ',c_commands{c},c_commands{c}));
+            system(sprintf('matlab -nodisplay -nosplash -nodesktop -r "addpath(''%s'');run %s;exit" -logfile /tmp/%s.log & ',pwd,c_commands{c},n));
         end
     end
+    [s,out]=system(list_active_compiles_cmd);out=strsplit(out,'\n');
     fprintf('Run following command to see when background compiles are done\n');
-    fprintf('ps -ef|grep -i matlab |grep %s |grep compile_command\n',getenv('USER'));
+    fprintf('%s',list_active_compiles_cmd);
+    fprintf('Trying to wait for completion automatically(noramlly takes less than 3 minutes).\n');
+    while size(out,2)>2
+        fprintf('.');
+        pause(5);
+        [s,out]=system(list_active_compiles_cmd);out=strsplit(out,'\n');
+    end
+    fprintf(' Done!\n');
+    fprintf('auto-wait seems to have worked!\n');
 end
 %%
-stop;
+return;
     % compile_command_cleanup % In dev, pulled from current use.
     compile_command_fid_splitter
     compile_command_gatekeeper
