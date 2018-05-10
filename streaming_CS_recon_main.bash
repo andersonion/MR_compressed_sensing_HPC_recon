6ef53574c85b1b444ac1d35a40d7b460d291c1fc
James Cook <jamesjcook@users.noreply.github.com>
2018-05-10 14:52:20 -0400

    added a force chunk_size if not set clause to our startup shell script.

--- streaming_CS_recon_main.bash
+++ streaming_CS_recon_main.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 # CS_streaming main code. Used to control the latest vs stable vs picka version code when running streaming main.
 # James cook 20171207
-echo $CS_CODE_DEV
 if [ -z "$CS_CODE_DEV" ]; then
     declare -x CS_CODE_DEV=stable;
 fi;
@@ -12,9 +11,15 @@ echo "Available exec versions...";
 ls -tr $WORKSTATION_HOME/matlab_execs/streaming_CS_recon_main_executable/
 exit;
 fi;
-echo "# start exec ($exec_path)"
-echo "# with args ($@)";
-$exec_path $@
+# added chucnk size force to try to make cs_recon easier on the slurm scheduler. 
+chunk_size_set=$(echo $@ |grep -c chunk_size);
+c_force="";
+if [ $chunk_size_set eq 0 ] ;then 
+    c_force="chunk_size=60";
+fi;
+echo "# start $CS_CODE_DEV exec ($exec_path)"
+echo "# with args ($@) $c_force";
+$exec_path $@ $c_force
 
 # advice on $@ and other things.
 # https://stackoverflow.com/questions/12314451/accessing-bash-command-line-args-vs
