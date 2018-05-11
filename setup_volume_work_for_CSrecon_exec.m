8d4b98921d09344bb96516f6c33ebdb294ecb157
James Cook <jamesjcook@users.noreply.github.com>
2018-05-11 17:15:46 -0400

    Added roll_data support. Testing for this feature incomplete and may break everything. -james WARNING: Potentially incomplete changes.

--- setup_volume_work_for_CSrecon_exec.m
+++ setup_volume_work_for_CSrecon_exec.m
@@ -5,11 +5,14 @@ function setup_volume_work_for_CSrecon_exec(setup_vars,volume_number)
    %aa
 %% Update of original version (implied _v1)
 if ~isdeployed
-    %{
-    setup_vars = '/nas4/bj/N55055.work//N55055_m0///work/N55055_m0_setup_variables.mat';
+    % {
+    roll_data = 1;
+    setup_vars = '/nas4/rja20/P55133.work/P55133_m000/work/P55133_m000_setup_variables.mat';
     volume_number = '1';
     addpath('/cm/shared/workstation_code_dev/recon/CS_v2/CS_utilities');
+    addpath('/cm/shared/workstation_code_dev/recon/CS_v2/sparseMRI_v0.2');
     %}
+
 end
 make_tmp = 0;
 %%   Import Variables
@@ -55,12 +58,17 @@ work_subfolder = fullfile(workdir,'work');
 volume_workspace_file = fullfile(work_subfolder,[volume_runno '_workspace.mat']);
 temp_file = [work_subfolder '/' volume_runno '.tmp'];
 if (starting_point == 2)
+    if isdeployed
     try
         dummy_mf = matfile(volume_workspace_file,'Writable',false);
         tmp_param = dummy_mf.param;
     catch
         make_workspace =1;
     end
+    else
+        make_workspace =1 ;
+    end
+    
     if ~exist(temp_file,'file')
         make_tmp = 1;
     end
@@ -96,9 +104,18 @@ if (make_workspace)
     fft_time=toc;
     log_msg =sprintf('Volume %s: Fourier transform along fully sampled dimension completed in %0.2f seconds.\n',volume_runno,fft_time);
     yet_another_logger(log_msg,log_mode,log_file);
+
+    m = matfile(recon_file,'Writable',true);
+    if ~isdeployed
+       options.roll_data=roll_data;
+    else
+       if ~isfield(options,'roll_data')
+          options.roll_data=0; 
+       end
+    end
     %% Calculate group scaling from first b0 image
-    if (~exist(scale_file,'file') && (volume_number==1))
-        [scaling, scaling_time] = calculate_CS_scaling(original_mask,data,original_pdf,original_dims(1));
+    if ((~exist(scale_file,'file') || (options.roll_data && ~isfield(m,'roll_keys'))) && (volume_number==1))
+        [scaling, scaling_time,shift_modifier] = calculate_CS_scaling(original_mask,data,original_pdf,original_dims(1),options.roll_data);
         %{
             tic
             current_slice=zeros([size(mask0)],'like',current_data);
@@ -114,12 +131,23 @@ if (make_workspace)
         %}
         log_msg =sprintf('Volume %s: volume scaling (%f) calculated in %0.2f seconds\n',volume_runno,scaling,scaling_time);
         yet_another_logger(log_msg,log_mode,log_file);
-        m = matfile(recon_file,'Writable',true);
+        %m = matfile(recon_file,'Writable',true); %moved outside of code
+        %block
         m.scaling = scaling;
+        
+        if (options.roll_data)
+        
+           m.roll_keys=shift_modifier; 
+            
+        end
         % Write scaling factor to scale file
         fid = fopen(scale_file,'w');
         fwrite(fid,scaling,'float');
         fclose(fid);
+    else
+        if (options.roll_data)
+           shift_modifier=m.roll_keys
+        end
     end
     %% Prep data for reconstruction
     % Calculate scaling
@@ -178,6 +206,22 @@ if (make_workspace)
     %% Save common variable file
     if ~exist(volume_workspace_file,'file')
         tic
+        
+        if (options.roll_data)
+            disp('Attempting to roll data via kspace...')
+            Ny=original_dims(2);
+            Nz=original_dims(3);
+            ky_profile=(shift_modifier(2)/(Ny))*(1:1:Ny);
+            kz_profile=(shift_modifier(3)/(Nz))*(1:1:Nz);         
+            [Kyy,Kzz]=meshgrid(ky_profile,kz_profile);
+            phase_matrix = exp(-2*pi*1i*(Kyy+Kzz));
+            phase_vector=phase_matrix(original_mask(:));
+            data=circshift(data,round(shift_modifier(1)));
+            for xx=1:original_dims(1);
+                data(xx,:)=phase_vector'.*data(xx,:);
+            end
+        end
+        
         real_data = real(data);
         imag_data = imag(data);
         savefast2(volume_workspace_file,'real_data','imag_data');
@@ -237,18 +281,75 @@ end
 
 end
 
-function [scaling,scaling_time] = calculate_CS_scaling(current_mask,current_data,mypdf0,n_slices)
+function [scaling,scaling_time,shift_modifier] = calculate_CS_scaling(current_mask,current_data,mypdf0,n_slices,roll_data)
+shift_modifier=[ 0 0 0 ];
 thresh = .999999999; % only clip out very noisy voxels (e.g. zippers, artifacts), and not the eyes
 tic
 current_slice=zeros([size(current_mask)],'like',current_data);
 qq=zeros([1,n_slices]);
+d1=1; % I'm never sure if how the dims map to the data as I imagine it...
+d2=2; % May need to swap these...
+if (roll_data)
+    y_sums=zeros([size(current_mask,d1),n_slices]);
+    z_sums=zeros([size(current_mask,d2),n_slices]);
+    x_sum=qq;
+    %y_sum=zeros([1, size(current_mask,d1)]);
+    %z_sum=zeros([1, size(current_mask,d2)]);
+end
 for n = 1:n_slices
     current_slice(current_mask(:))=current_data(n,:);
     temp_data = abs(ifftn(current_slice./mypdf0)); % 8 May 2017, BJA: Don't need to waste computations on fftshift for scaling calculation
     qq(n)= max(temp_data(:));%quantile(temp_data(:),thresh);
+    if (roll_data)   
+        y_sums(:,n)=mean(temp_data,d2);
+        z_sums(:,n)=mean(temp_data,d1)';
+        x_sum(n)=mean(temp_data(:));
+    end
 end
 
 q = quantile(qq,thresh);
+if (roll_data)
+    x_sum=x_sum';
+    y_sum=mean(y_sums,2);
+    z_sum=mean(z_sums,2);
+    
+    [x_min,first_corner_voxel(1)] = min(x_sum);
+    [y_min,first_corner_voxel(2)] = min(y_sum);
+    [z_min,first_corner_voxel(3)] = min(z_sum);
+    
+    vs=[n_slices size(current_mask)];
+    vc=zeros(1,3);
+    center=vc;
+    vc=round(vs/2);
+    center=first_corner_voxel+vc-1;
+    center(center>vs)=center(center>vs)-vs(center>vs);
+    shift_modifier=vc-center;
+    
+    x_sum=circshift(x_sum,round(shift_modifier(1)));
+    y_sum=circshift(y_sum,round(shift_modifier(2)));
+    z_sum=circshift(z_sum,round(shift_modifier(3)));
+    
+    first_3rd_idx = round(vs/3);
+    last_3rd_idx = round(2*vs/3)+1;
+    cof=1.1; % Cof=> "coeffecient"
+    lmin(1)=find((x_sum(1:first_3rd_idx(1))<cof*x_min),1,'last');
+    lmin(2)=find((y_sum(1:first_3rd_idx(2))<cof*y_min),1,'last');
+    lmin(3)=find((z_sum(1:first_3rd_idx(3))<cof*z_min),1,'last');
+    
+    umin(1)=find((x_sum(last_3rd_idx(1):end)<cof*x_min),1);
+    umin(2)=find((y_sum(last_3rd_idx(2):end)<cof*y_min),1);
+    umin(3)=find((z_sum(last_3rd_idx(3):end)<cof*z_min),1);
+    
+    umin=umin+last_3rd_idx-1;
+    
+    new_c=round((umin-lmin)/2+lmin);
+    center=new_c-shift_modifier;% adjust new center back for our original position.
+    center(center>vs)=center(center>vs)-vs(center>vs);
+    first_corner_voxel=center+1-vc;
+    first_corner_voxel(first_corner_voxel<0)=first_corner_voxel(first_corner_voxel<0)+vs(first_corner_voxel<0);
+
+    shift_modifier=vc-center; 
+end
 scaling_time = toc;
 scaling = (2^16-1)/q; % we plan on writing out uint16 not int16, though it won't show up well in the database
 scaling = double(scaling);
