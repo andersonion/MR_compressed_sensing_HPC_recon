7adb360d3fa1da9005d48628fe108bef0f52e65a
James Cook <jamesjcook@users.noreply.github.com>
2018-05-16 13:08:26 -0400

    Refined the final 2% of completion to separate out send images from headfile. Renamed some find flag variables to match their flag files on disk. Added simple resolve wildcard function for flag files. Could do a better job making this unnecessary, but that refinement is for later.

--- CS_utilities/check_status_of_CSrecon.m
+++ CS_utilities/check_status_of_CSrecon.m
@@ -40,19 +40,34 @@ starting_point = 6;
 % recon_completed, but that has since been changed.
 % vflag_name = sprintf('.%s.recon_completed',volume_runno);
 % archive_ready_flag = fullfile(volume_dir,vflag_name);
-archive_ready_flag = fullfile(volume_dir,sprintf('%simages',volume_runno), ...
+send_archive_tag = fullfile(volume_dir,sprintf('%simages',volume_runno), ...
     sprintf('.%s_send_archive_tag_to_*_SUCCESSFUL',volume_runno));
-[s,archive_ready_flag]=system(sprintf('ls -t %s|head -n2|tail -n1',archive_ready_flag));
-if s ~= 0
-    archive_ready_flag='__FIND_FLAG_ERROR'
-end
-archive_ready_flag=strtrim(archive_ready_flag);
+
+send_archive_tag=wildcard_tag_finder(send_archive_tag);
+
 vol_status=100;% starting at complete, keep working it down. 
 % vol status is what % from 0-100 are we. 
 % have to assign each stage some % it takes. lets pretend that only slices matter, and they take 90% of the work.
-if ~exist(archive_ready_flag,'file')
+if ~exist(send_archive_tag,'file')
     starting_point = 5;
-    vol_status=vol_status-2;
+    vol_status=vol_status-0.1;% this represents the last flag to send.
+    send_images=fullfile(volume_dir,sprintf('%simages',volume_runno), ...
+        sprintf('.%s_send_images_to_*_SUCCESSFUL',volume_runno));
+    send_images=wildcard_tag_finder(send_images);
+    if ~exist(send_images,'file')
+        vol_status=vol_status-1.8;
+    end
+    %send_headfile='FILLMEINTWO';.T00020_m0_send_headfile_to_delos_SUCCESSFUL
+    send_headfile=fullfile(volume_dir,sprintf('%simages',volume_runno), ...
+        sprintf('.%s_send_headfile_to_*_SUCCESSFUL',volume_runno));
+    send_headfile=wildcard_tag_finder(send_headfile);
+    if ~exist(send_headfile,'file')
+        vol_status=vol_status-0.1;
+    end
+    % this 2 pct represents processing the procpar, and images and headfile
+    % to dest engine. We could break this down further, but its probably
+    % not worth it. eg, 1.8% is send data, 0.1% is proces procpar, 0.1% is
+    % send procpar.
     % Check for output images
     images_dir = fullfile(volume_dir,[volume_runno 'images']);
     if ~exist(images_dir,'dir')
@@ -122,7 +137,7 @@ if ~exist(archive_ready_flag,'file')
                 volume_fid=fullfile(work_subfolder,[volume_runno,'.fid']);
                 if ~exist(volume_fid,'file')
                     starting_point = 1;
-		    vol_status=vol_status-4
+                    vol_status=vol_status-4;
                     % Need to remember to  handle differently for single
                     % blocks scans...I think (I haven't put in the split code for this yet!).
                     if (exist('scanner','var') && exist('runno','var') && exist('study','var') && exist('agilent_series','var'))
@@ -151,14 +166,15 @@ end
 if starting_point==0
     vol_status=vol_status-1;
 end
-status_codes={'Acquistion in progress on scanner.'
-'Extract fid.'
-'Run volume setup. (create workspace.mat and .tmp files)'
-'Slice jobs.'
-'volume cleanup.'
-'Send volume to workstation and write recon_completed flag.'
-'All work done; do nothing.'
-};
+status_codes={...
+    'Acquistion in progress on scanner.'
+    'Extract fid.'
+    'Run volume setup. (create workspace.mat and .tmp files)'
+    'Slice jobs.'
+    'volume cleanup.'
+    'Send volume to workstation and write recon_completed flag.'
+    'All work done; do nothing.'
+    };
 % vol_status=sprintf('%s',status_codes{starting_point+1},vol_status);
 log_msg =sprintf('Starting point for volume %s: Stage %i. %s\n',volume_runno,starting_point,status_codes{starting_point+1});
 
@@ -166,3 +182,10 @@ log_msg =sprintf('Starting point for volume %s: Stage %i. %s\n',volume_runno,sta
 
 end
 
+function resolved_tag=wildcard_tag_finder(tag_pattern)
+[s,tag_pattern]=system(sprintf('ls -t %s|head -n2|tail -n1',tag_pattern));
+if s ~= 0
+    tag_pattern='__FIND_FLAG_ERROR';
+end
+resolved_tag=strtrim(tag_pattern);
+end
