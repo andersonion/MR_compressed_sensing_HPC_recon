b773d34a517054a8a62b59f1b12bcc2f53691e7e
James Cook <jamesjcook@users.noreply.github.com>
2018-07-10 15:02:45 -0400

    Added get_reservation function to required code.

--- compile_command_streaming_CSrecon_main.m
+++ compile_command_streaming_CSrecon_main.m
@@ -1,11 +1,12 @@
 %compile me
 function compile_command_streaming_CSrecon_main
 %exec_env_var='';%optional shell env var to be cleared.
-include_files = {'/cm/shared/workstation_code_dev/recon/CS_v2/gui_info_collect.m' ...    
-    '/cm/shared/workstation_code_dev/recon/CS_v2/puller_glusterspaceCS_2.m' ...
-    '/cm/shared/apps/MATLAB/R2015b/toolbox/signal/signal/hamming.m' ...
-    '/cm/shared/apps/MATLAB/R2015b/toolbox/images/images/padarray.m' ...
-    '/cm/shared/workstation_code_dev/recon/CS_v2/zpad.m'...
+include_files = {'/cm/shared/workstation_code_dev/recon/CS_v2/gui_info_collect.m'     
+    '/cm/shared/workstation_code_dev/recon/CS_v2/puller_glusterspaceCS_2.m' 
+    '/cm/shared/apps/MATLAB/R2015b/toolbox/signal/signal/hamming.m' 
+    '/cm/shared/apps/MATLAB/R2015b/toolbox/images/images/padarray.m' 
+    '/cm/shared/workstation_code_dev/recon/CS_v2/zpad.m'
+    '/cm/shared/workstation_code_dev/recon/CS_v2/CS_utilities/get_reservation.m'
     };%optional, but required if using exec_env_var, can be empty.
 compile_dir=compile_command__allpurpose('streaming_CS_recon_main_exec.m',include_files);%,exec_env_var);
 
--- compile_command_volume_manager.m
+++ compile_command_volume_manager.m
@@ -4,5 +4,6 @@ exec_env_var='CS_VOLUME_MANAGER_EXEC';
 include_files = {'/cm/shared/workstation_code_dev/recon/CS_v2/CS_utilities/read_header_of_CStmp_file.m'
   '/cm/shared/workstation_code_dev/recon/CS_v2/CS_utilities/ssh_call.m'
   '/cm/shared/workstation_code_dev/recon/CS_v2/deploy_procpar_handlers.m'
+  '/cm/shared/workstation_code_dev/recon/CS_v2/CS_utilities/get_reservation.m'
   };
 compile_command__allpurpose('volume_manager_exec.m',include_files,exec_env_var);
