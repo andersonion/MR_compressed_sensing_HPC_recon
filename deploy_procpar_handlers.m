1f199851f97fa2f77fff9e2fa338d95836b46025
James Cook <jamesjcook@users.noreply.github.com>
2018-07-10 16:54:20 -0400

    Removed old commented code, and added blank reservation to slurm settings.

--- deploy_procpar_handlers.m
+++ deploy_procpar_handlers.m
@@ -2,10 +2,6 @@ function [ c_running_jobs ] = deploy_procpar_handlers(volume_variable_file)
 %Handles waiting for the procpar file to exist, then processing it into a
 %complete headfile.
  
-if ~isdeployed
-    volume_variable_file='/civmnas4/cof/N56021.work/N56021_m16/N56021_m16_setup_variables.mat';
-    %handle_archive_tag_cmd='echo "Hello, Mofo."';
-end
 % TEMPORARY CODE for backwards compatibility of in-progress scans remove by
 % June 12th, 2018
 if ~exist(volume_variable_file,'file')
@@ -93,6 +89,8 @@ ppcu_slurm_options.p='slow_master'; % For now, will use gatekeeper queue for vol
 %ppcu_slurm_options.job_name = [volume_runno '_procpar_cleanup'];
 ppcu_slurm_options.job_name = [runno '_procpar_gatekeeper_and_processor']; % Trying singleton dependency
 %ppcu_slurm_options.reservation = active_reservation;
+% using a blank reservation to force no reservation for this job.
+ppcu_slurm_options.reservation = '';
 procpar_cleanup_batch = [workdir 'sbatch/' volume_runno '_procpar_cleanup.bash'];
 ppcu_cmd = sprintf('%s %s %s %s %s %s', procpar_cleanup_exec_path,matlab_path, recon_file,headfile,procpar_file,recon_type );
 dep_string ='';
@@ -103,29 +101,3 @@ if ~options.keep_work
     batch_file = create_slurm_batch_files(procpar_cleanup_batch,{ppcu_cmd ship_cmd_0 ship_cmd_1 ship_cmd_2 write_hf_success_cmd handle_archive_tag_cmd},ppcu_slurm_options);
     c_running_jobs = dispatch_slurm_jobs(batch_file,'','','singleton');
 end
-
-%%% 30 May 2018 -- Move to volume cleanup
-%{
-if ~options.keep_work && ~options.process_headfiles_only
-    %%%% Schedule cleanup
-
-    trashman_slurm_options=struct;
-    trashman_slurm_options.v=''; % verbose
-    trashman_slurm_options.s=''; % shared; volume manager needs to share resources.
-    trashman_slurm_options.mem=500; % memory requested; trashman only needs a miniscule amount.
-    trashman_slurm_options.p='slow_master';
-    trashman_slurm_options.job_name = [volume_runno '_trashman']; % Trying singleton dependency
-    trashman_batch = [workdir 'sbatch/' volume_runno '_trashman.bash'];
-    trashman_cmd = sprintf('if [[ -f "%s" ]]; then\n\tif [[ -d "%s" ]]; then\n\t\techo "Images have been successfully transferred; removing %s now...";\n\t\trm -rf %s;\n\telse\n\t\techo "Work folder %s already appears to have been removed. No action will be taken.";\n\tfi\nelse\n\techo "Images have not been successfully transferred yet; work folder will not be removed at this time.";\nfi', success_flag,work_subfolder,work_subfolder,work_subfolder,work_subfolder );
-    dep_string ='';
-    if stage_5_running_jobs
-        dep_string = 'afterok-or';
-    end
-    batch_file = create_slurm_batch_files(trashman_batch,trashman_cmd, trashman_slurm_options);
-    c_running_jobs = dispatch_slurm_jobs(batch_file,'',stage_5_running_jobs ,dep_string);
-    log_mode = 1;
-    log_msg =sprintf('Once images for volume %s have been reconned and sent to %s, work folder %s will be removed via SLURM job(s): %s.\n',volume_runno,target_machine,work_subfolder,c_running_jobs);
-    yet_another_logger(log_msg,log_mode,log_file);
-   
-end
-%}
