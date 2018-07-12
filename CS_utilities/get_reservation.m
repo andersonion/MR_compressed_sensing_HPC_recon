132624d7558c1a6ef60b95677b225f331ccbcb1c
James Cook <jamesjcook@users.noreply.github.com>
2018-07-12 14:39:09 -0400

    Fixed bug where we could get a bogus setenv if we had no reservation.

--- CS_utilities/get_reservation.m
+++ CS_utilities/get_reservation.m
@@ -1,21 +1,20 @@
 function active_reservation=get_reservation(resopt)
+% resopt is a logical or string 
 % This should work fine, even if CS_reservation is not set.
+% it will just be a blank in that case.
 active_reservation=getenv('CS_reservation'); 
 
 % normal system variables, if these are set we should use these and ignore
 % what the user said, empty strings are unset.
 sbatch_r=getenv('SBATCH_RESERVATION');
-slurm_r =getenv('SLURM_RESERVATION'); 
-
-if ~isempty(sbatch_r)
+slurm_r =getenv( 'SLURM_RESERVATION');
+if ~isempty(resopt) && ischar(resopt) 
+    active_reservation=resopt;
+elseif ~isempty(sbatch_r)
     active_reservation=sbatch_r;
 elseif ~isempty(slurm_r)
     active_reservation=slurm_r;
-elseif ~islogical(resopt)
-    active_reservation=resopt;
 end
-setenv('CS_reservation',active_reservation);
-
 % Ensure that reservation exists
 if (active_reservation)
     [~, res_check] = system(['scontrol show reservation ' active_reservation]);
@@ -25,3 +24,4 @@ if (active_reservation)
         active_reservation = '';
     end
 end
+setenv('CS_reservation',active_reservation);
