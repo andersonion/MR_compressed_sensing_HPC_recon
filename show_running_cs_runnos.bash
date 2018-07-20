903c0b4b2d97197ed1c4cc77bca63397c72b78f2
James Cook <jamesjcook@users.noreply.github.com>
2018-07-20 10:59:14 -0400

    Made var consistent with other helpers.

--- show_running_cs_runnos.bash
+++ show_running_cs_runnos.bash
@@ -5,7 +5,7 @@ the_user=$USER;
 if [ ! -z "$1" ];then
     the_user=$1;
 fi;
-hlper_dir="$WKS_HOME/recon/CS_v2";
-stat_queue=$("$hlper_dir/capture_squeue.bash" $the_user);
+hlpr_dir="$WKS_HOME/recon/CS_v2";
+stat_queue=$("$hlpr_dir/capture_squeue.bash" $the_user);
 
 for jn in $(cat $stat_queue |cut -b 25-68 |xargs); do echo $jn|cut -b 1-6; done |sort -u
