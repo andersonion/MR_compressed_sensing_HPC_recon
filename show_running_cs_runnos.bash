2f02fe09452cb2c0a85cc5bbcac83d2f0348d4bf
James Cook <jamesjcook@users.noreply.github.com>
2018-07-24 12:09:50 -0400

    Moved capture_squeue to its better place in pipeline_utiltities. It was also renamed to slurm_queue_capture.bash and added to binlinking of installer.

--- show_running_cs_runnos.bash
+++ show_running_cs_runnos.bash
@@ -6,6 +6,6 @@ if [ ! -z "$1" ];then
     the_user=$1;
 fi;
 hlpr_dir="$WKS_HOME/recon/CS_v2";
-stat_queue=$("$hlpr_dir/capture_squeue.bash" $the_user);
+stat_queue=$("slurm_queue_snapshot" $the_user);
 
 for jn in $(cat $stat_queue |cut -b 25-68 |xargs); do echo $jn|cut -b 1-6; done |sort -u
