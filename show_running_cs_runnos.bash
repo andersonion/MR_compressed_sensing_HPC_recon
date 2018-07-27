54da8a592dbd69b02950e6043f5b6c8b6ec617e2
James Cook <jamesjcook@users.noreply.github.com>
2018-07-27 11:43:48 -0400

    Fixed fitler code to run much faster. Horray!

--- show_running_cs_runnos.bash
+++ show_running_cs_runnos.bash
@@ -5,7 +5,10 @@ the_user=$USER;
 if [ ! -z "$1" ];then
     the_user=$1;
 fi;
-hlpr_dir="$WKS_HOME/recon/CS_v2";
-stat_queue=$("slurm_queue_snapshot" $the_user);
 
-for jn in $(cat $stat_queue |cut -b 25-68 |xargs); do echo $jn|cut -b 1-6; done |sort -u
+stat_queue=$("slurm_queue_snapshot" $the_user);
+echo "queue_capture=$stat_queue"
+# old slower version
+#for jn in $(cat $stat_queue |cut -b 25-68 |xargs); do echo $jn|cut -b 1-6; done |sort -u
+awk '{print $3}' $stat_queue |sed -E 's/.*([A-Z][0-9]+).*/\1/g'|sort -u
+
