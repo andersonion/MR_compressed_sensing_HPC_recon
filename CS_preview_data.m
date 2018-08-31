09166b2ebda2de2b9d8d32a3fbb75c0ab87bbc68
James Cook <jamesjcook@users.noreply.github.com>
2018-08-31 17:28:01 -0400

    Added horrifing save full volume code.

--- CS_preview_data.m
+++ CS_preview_data.m
@@ -1,6 +1,28 @@
-function CS_preview_data(original_mask,data,out_base)
+function img=CS_preview_data(original_mask,data,out_base,mode)
+% mode, slice or volume
+mtxt='';
+if isempty(regexp(mode,'volume','ONCE'))
+    %mode text
+    mtxt='_ortho';
+end
+img{1}=sprintf('%s_1_kspace_ortho.nii',out_base);
+img{2}=sprintf('%s_2_imgspace%s.nii',out_base,mtxt);
+img{3}=sprintf('%s_3_kspace_inv_ortho.nii',out_base);
+% setting this to less than nimages will prevent figure display
+fig_start=-30;
+work=numel(img);
+for i_n=1:numel(img)
+    if exist(img{i_n},'file')
+        work=work-1;
+    end
+end
+if ~work 
+    disp('previews dumped');
+    return;
+end
+
 original_dims=[size(data,1),size(original_mask)];
-lil_dummy = zeros([1,1],'double');
+lil_dummy = zeros([1,1],'single');
 lil_dummy = complex(lil_dummy,lil_dummy);
 temp_data=zeros(original_dims,'like',lil_dummy);
 % populate full array with all the data.
@@ -8,11 +30,18 @@ for n = 1:original_dims(1)
     temp_data(n,original_mask(:))=data(n,:);
 end
 % save basic kspace preview
-disp_vol_center(temp_data,1,301,sprintf('%s_1_kspace_ortho.tif',out_base));
+disp_vol_center(temp_data,1,fig_start+1,img{1});
 %% get transform of ksapce to see base image.
 temp_data=fftshift(fftshift(ifft(ifft(fftshift(fftshift(temp_data,2),3),[],2),[],3),2),3);
-disp_vol_center(abs(temp_data),0,302,sprintf('%s_2_imgspace_ortho.png',out_base));
+if ~isempty(regexpi(mode,'volume'))
+    save_nii(make_nii(abs(temp_data)),img{2});
+else
+    disp_vol_center(abs(temp_data),0,fig_start+2,img{2});
+end
 %% invert transform again to see fully populated kspace
+if exist(img{3},'file')
+    return;
+end
 temp_data=fftshift(ifftn(fftshift(temp_data)));
 %{
 temp_data=fftshift(fftshift(fftshift(...
@@ -21,5 +50,5 @@ temp_data=fftshift(fftshift(fftshift(...
     ,[],1),[],2),[],3)...
     ,1),2),3);
 %}
-disp_vol_center(temp_data,1,303,sprintf('%s_3_kspace_inv_ortho.tif',out_base));
+disp_vol_center(temp_data,1,fig_start+3,img{3});
 end
