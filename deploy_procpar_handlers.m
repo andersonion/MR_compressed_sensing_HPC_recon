27c763c14f8d0d4d350dcd60c99b82378cd2e029
James Cook <jamesjcook@users.noreply.github.com>
2018-09-28 16:53:31 -0400

    Adjusted to use current user instead of omege for ssh.

--- deploy_procpar_handlers.m
+++ deploy_procpar_handlers.m
@@ -1,9 +1,10 @@
 function [ c_running_jobs ] = deploy_procpar_handlers(volume_variable_file)
-%Handles waiting for the procpar file to exist, then processing it into a
-%complete headfile.
+% Handles waiting for the procpar file to exist,
+% then processing it into a complete headfile.
  
 % TEMPORARY CODE for backwards compatibility of in-progress scans remove by
 % June 12th, 2018
+%{
 if ~exist(volume_variable_file,'file')
     [t_workdir, t_file_name, t_ext]=fileparts(volume_variable_file);
     old_vv_file = [t_workdir '/work/' t_file_name t_ext];
@@ -12,14 +13,20 @@ if ~exist(volume_variable_file,'file')
         system(mv_cmd);
     end
 end
+%}
+%% OPAQUE LOAD OF A BUNCH OF THINGS
 load(volume_variable_file)
+%%%%%%
 
-hf_fail_flag=         sprintf('%s/.%s_send_headfile_to_%s_FAILED',        images_dir,volume_runno,target_machine);
-hf_success_flag=      sprintf('%s/.%s_send_headfile_to_%s_SUCCESSFUL',    images_dir,volume_runno,target_machine);
+%%
+
+hf_fail_flag=         sprintf('%s/.%s_send_headfile_to_%s_FAILED', ...
+    images_dir,volume_runno,target_machine);
+hf_success_flag=      sprintf('%s/.%s_send_headfile_to_%s_SUCCESSFUL', ...
+    images_dir,volume_runno,target_machine);
 
 full_host_name=sprintf('%s.dhe.duke.edu',target_machine);
 
-log_mode=1;
 recon_type = 'CS_v2';
 matlab_path = '/cm/shared/apps/MATLAB/R2015b/';
 
@@ -27,12 +34,13 @@ gatekeeper_queue = getenv('CS_GATEKEEPER_QUEUE');
 if isempty(gatekeeper_queue)
     gatekeeper_queue = 'slow_master';%'high_priority';
 end
+%% exec path setting
 % set an env var to get latest dev code, or will defacto run stable.
 CS_CODE_DEV=getenv('CS_CODE_DEV');
 if isempty(CS_CODE_DEV)
     CS_CODE_DEV='stable';
 end
-procpar_gatekeeper_exec_path = getenv('CS_PROCPAR_GATEKEEPER_EXEC'); % Error check for isempty?
+procpar_gatekeeper_exec_path = getenv('CS_PROCPAR_GATEKEEPER_EXEC'); 
 if isempty(procpar_gatekeeper_exec_path)
     procpar_gatekeeper_exec_path =['/cm/shared/workstation_code_dev/matlab_execs/local_file_gatekeeper_executable/' CS_CODE_DEV '/run_local_file_gatekeeper_exec.sh'];
     setenv('CS_PROCPAR_GATEKEEPER_EXEC',procpar_gatekeeper_exec_path);
@@ -43,14 +51,34 @@ if isempty(procpar_cleanup_exec_path)
     setenv('CS_PROCPAR_CLEANUP_EXEC',procpar_cleanup_exec_path);
 end
 
+%%
 % Send a message that all recon is completed and has successfully
 % been sent to the target machine
-
+%% build shell commnands that do work.
 ship_cmd_0=sprintf('if [[ -f %s ]]; then\n\trm %s;\nfi',hf_fail_flag,hf_fail_flag);
-ship_cmd_1 = sprintf('ssh omega@%s ''if [[ ! -d /Volumes/%sspace/%s/ ]] ; then\n\t mkdir -m 777 /Volumes/%sspace/%s/;\nfi;''\nscp -p %s omega@%s:/Volumes/%sspace/%s/;',full_host_name,target_machine,volume_runno,target_machine,volume_runno,procpar_file,full_host_name,target_machine,volume_runno);
-ship_cmd_2 = sprintf('hf_success=0;\nssh omega@%s ''if [[ ! -d /Volumes/%sspace/%s/%simages/ ]] ; then\n\t mkdir -m 777 /Volumes/%sspace/%s/%simages/;\nfi '';\nscp -p %s omega@%s:/Volumes/%sspace/%s/%simages/ && hf_success=1',full_host_name,target_machine,volume_runno,volume_runno,target_machine,volume_runno, volume_runno,headfile,full_host_name,target_machine,volume_runno,volume_runno);
-write_hf_success_cmd = sprintf('if [[ $hf_success -eq 1 ]];\nthen\n\techo "Headfile transfer successful!"\n\ttouch %s;\nelse\n\ttouch %s; \nfi',hf_success_flag,hf_fail_flag);
+ship_cmd_1 = sprintf(['ssh %s@%s ''if [[ ! -d /Volumes/%sspace/%s/ ]] ; then\n'...
+    '\t mkdir -m 777 /Volumes/%sspace/%s/;\n'...
+    'fi;''\n'...
+    'scp -p %s %s@%s:/Volumes/%sspace/%s/;'],...
+    getenv('USER'),full_host_name,target_machine,volume_runno,target_machine,volume_runno,...
+    procpar_file,getenv('USER'),full_host_name,target_machine,volume_runno);
+ship_cmd_2 = sprintf(['hf_success=0;\n'...
+    'ssh %s@%s ''if [[ ! -d /Volumes/%sspace/%s/%simages/ ]] ; then\n'...
+    '\t mkdir -m 777 /Volumes/%sspace/%s/%simages/;\n'...
+    'fi '';\n'...
+    'scp -p %s %s@%s:/Volumes/%sspace/%s/%simages/ && hf_success=1'],...
+    getenv('USER'),full_host_name,target_machine,volume_runno,volume_runno,target_machine,volume_runno, volume_runno,...
+    headfile,getenv('USER'),full_host_name,target_machine,volume_runno,volume_runno);
+write_hf_success_cmd = sprintf(['if [[ $hf_success -eq 1 ]];\n'...
+    'then\n'...
+    '\techo "Headfile transfer successful!"\n'...
+    '\ttouch %s;\n'...
+    'else\n'...
+    '\ttouch %s; \n'...
+    'fi'],hf_success_flag,hf_fail_flag);
 
+%% check current acq status
+% get procpar file if its missing, and we can.
 %pp_running_jobs='';
 [~, local_or_streaming_or_static]=find_input_fidCS(scanner,runno,study,agilent_series);
 if ~exist(procpar_file,'file') ...
@@ -60,7 +88,8 @@ if ~exist(procpar_file,'file') ...
     puller_glusterspaceCS_2(runno,datapath,scanner,base_workdir,mode);
 end
 
-
+%% set up procpar gatekeeper
+% uses singleton to hold the next job behind it.
 if (~exist(procpar_file,'file') || ~exist(headfile,'file'))
     gk_slurm_options=struct;
     gk_slurm_options.v=''; % verbose
@@ -81,11 +110,12 @@ if (~exist(procpar_file,'file') || ~exist(headfile,'file'))
     log_msg =sprintf('Procpar data and/or headfile for volume %s will be processed as soon as it is available; initializing gatekeeper (SLURM jobid(s): %s).\n',volume_runno,pp_running_jobs);
     yet_another_logger(log_msg,log_mode,log_file);
 end
+%% 
 ppcu_slurm_options=struct;
 ppcu_slurm_options.v=''; % verbose
-ppcu_slurm_options.s=''; % shared; volume manager needs to share resources.
+ppcu_slurm_options.s=''; % shared; 
 ppcu_slurm_options.mem=500; % memory requested; ppcu only needs a miniscule amount.
-ppcu_slurm_options.p='slow_master'; % For now, will use gatekeeper queue for volume manager as well
+ppcu_slurm_options.p='slow_master'; % For now, will use gatekeeper queue 
 %ppcu_slurm_options.job_name = [volume_runno '_procpar_cleanup'];
 ppcu_slurm_options.job_name = [runno '_procpar_gatekeeper_and_processor']; % Trying singleton dependency
 %ppcu_slurm_options.reservation = active_reservation;
@@ -97,7 +127,10 @@ dep_string ='';
 %if pp_running_jobs
 %   dep_string = 'afterok';
 %end
-if ~options.keep_work
+
+% 2018-09-28 its not clear the keep_work setting should have an effect
+% here. the if has been disabled. --james
+%if ~options.keep_work
     batch_file = create_slurm_batch_files(procpar_cleanup_batch,{ppcu_cmd ship_cmd_0 ship_cmd_1 ship_cmd_2 write_hf_success_cmd handle_archive_tag_cmd},ppcu_slurm_options);
     c_running_jobs = dispatch_slurm_jobs(batch_file,'','','singleton');
-end
+%end
