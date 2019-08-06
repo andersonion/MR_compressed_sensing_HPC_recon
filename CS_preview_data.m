c06070b69fb51bdc9b2ccf21cfd3aea2a2f772b6
James Cook <jamesjcook@users.noreply.github.com>
2019-08-06 13:19:54 -0400

    Updated to allow volume save of kspace

--- CS_preview_data.m
+++ CS_preview_data.m
@@ -1,13 +1,17 @@
 function img=CS_preview_data(original_mask,data,out_base,mode)
 % mode, slice or volume
 mtxt='';
-if isempty(regexp(mode,'volume','ONCE'))
+if isempty(regexpi(mode,'volume','ONCE'))
     %mode text
     mtxt='_ortho';
 end
-img{1}=sprintf('%s_1_kspace_ortho.nii',out_base);
+img{1}=sprintf('%s_1_kspace%s.nii',out_base,mtxt);
 img{2}=sprintf('%s_2_imgspace%s.nii',out_base,mtxt);
 img{3}=sprintf('%s_3_kspace_inv_ortho.nii',out_base);
+if ~isempty(regexpi(mode,'volume'))
+    img{1}=sprintf('%s_1_kspace%s.nii',out_base,'_re');
+    img{4}=sprintf('%s_1_kspace%s.nii',out_base,'_im');
+end
 % setting this to less than nimages will prevent figure display
 fig_start=-30;
 work=numel(img);
@@ -30,7 +34,12 @@ for n = 1:original_dims(1)
     temp_data(n,original_mask(:))=data(n,:);
 end
 % save basic kspace preview
-disp_vol_center(temp_data,1,fig_start+1,img{1});
+if ~isempty(regexpi(mode,'volume'))
+    save_nii(make_nii(real(temp_data)),img{1});
+    save_nii(make_nii(imag(temp_data)),img{4});
+else
+    disp_vol_center(temp_data,1,fig_start+1,img{1});
+end
 %% get transform of ksapce to see base image.
 temp_data=fftshift(fftshift(ifft(ifft(fftshift(fftshift(temp_data,2),3),[],2),[],3),2),3);
 if ~isempty(regexpi(mode,'volume'))
