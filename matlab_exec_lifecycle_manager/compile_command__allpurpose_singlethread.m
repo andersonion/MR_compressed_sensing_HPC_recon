d70622dc132d34af1884f910c3794c6f32dbef04
James Cook <jamesjcook@users.noreply.github.com>
2019-08-07 14:36:31 -0400

    Removed matlab_exec_lifecycle_manager code now that it has its own repo

--- matlab_exec_lifecycle_manager/compile_command__allpurpose_singlethread.m
+++
@@ -1,4 +0,0 @@
-function compile_dir=compile_command__allpurpose_singlethread(source_filename,include_files,exec_env_var)
-% see help for compile_comman__allpurpose
-% this just forces the singlethreaded option into mcc
-compile_dir=compile_command__allpurpose(source_filename,include_files,exec_env_var,'-R -singleCompThread');
