eba64d6caaa6514294fde133d3569e3470e5461a
James Cook <jamesjcook@users.noreply.github.com>
2019-08-07 14:28:49 -0400

    updated paths in compile helper

--- compile_commands/compile__pathset.m
+++ compile_commands/compile__pathset.m
@@ -5,7 +5,7 @@ addpath(fullfile(pdir,'sparseMRI_v0.2'));
 addpath(fullfile(pdir,'sparseMRI_v0.2/simulation'));
 addpath(fullfile(pdir,'sparseMRI_v0.2/threshold'));
 addpath(fullfile(pdir,'sparseMRI_v0.2/utils'));
-addpath(fullfile(pdir,'testing_and_prototyping'));
+%addpath(fullfile(pdir,'testing_and_prototyping'));
 addpath(fullfile(pdir,'utility'));
 % common_utils had to be added for cs_recon main
 addpath([getenv('WORKSTATION_HOME') '/shared/civm_matlab_common_utils']);
@@ -22,8 +22,9 @@ addpath([getenv('WORKSTATION_HOME') '/shared/mathworks/extrema/']);
 addpath([getenv('WORKSTATION_HOME') '/shared/mathworks/hist2/']);
 addpath([getenv('WORKSTATION_HOME') '/shared/mathworks/multiecho_enhance/']);
 addpath([getenv('WORKSTATION_HOME') '/shared/mathworks/NIFTI_20140122/']);
+addpath([getenv('WORKSTATION_HOME') '/shared/mathworks/nrrdWriter/']);
+addpath([getenv('WORKSTATION_HOME') '/shared/mathworks/nrrdread/']);
 addpath([getenv('WORKSTATION_HOME') '/shared/mathworks/resize/']);
 addpath([getenv('WORKSTATION_HOME') '/shared/mathworks/slurm/']);
 addpath([getenv('WORKSTATION_HOME') '/shared/mathworks/wildcardsearch/']);
-addpath([getenv('WORKSTATION_HOME') '/shared/mathworks/zips/']);
 % 
