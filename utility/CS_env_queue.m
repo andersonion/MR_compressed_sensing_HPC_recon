3a4d7630b2275d3e6ee2b075a715ff3c3f1f4c92
James Cook <jamesjcook@users.noreply.github.com>
2020-02-07 10:16:10 -0500

    Fixed glitch setting wrong queues

--- utility/CS_env_queue.m
+++ utility/CS_env_queue.m
@@ -11,10 +11,10 @@ end
 cs_queue.full_volume = getenv('CS_FULL_VOLUME_QUEUE');
 if isempty(cs_queue.full_volume)
     cs_queue.full_volume = 'high_priority';
-    setenv('CS_FULL_VOLUME_QUEUE',cs_queue.gatekeeper)
+    setenv('CS_FULL_VOLUME_QUEUE',cs_queue.full_volume)
 end
 cs_queue.recon = getenv('CS_RECON_QUEUE');
 if isempty(cs_queue.recon)
     cs_queue.recon = 'matlab';
-    setenv('CS_RECON_QUEUE',cs_queue.gatekeeper)
+    setenv('CS_RECON_QUEUE',cs_queue.recon)
 end
