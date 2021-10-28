8cc394fa136f352bd5ad48afe8a37d0adbc4fc7c
James Cook <jamesjcook@users.noreply.github.com>
2021-10-28 14:48:58 -0400

    Added example extract_info_from_CStable

---
+++ example/example_extract_info_from_CStable.m
@@ -0,0 +1,4 @@
+% test_extract_info_from_CStablem
+
+procpar='/mnt/civmbigdata/civmBigDataVol/jjc29/S68194.work/S68194.procpar'
+[skiptable, dim2, dim3, pa, pb] = extract_info_from_CStable(procpar);
