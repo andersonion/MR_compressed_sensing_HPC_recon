c28be726f2dc6bfbef43402a3486f5d78de51690
James Cook <jamesjcook@users.noreply.github.com>
2024-02-07 14:41:36 -0500

    Now can update paths with a base dir when specified to grant full paths

--- utility/load_index_file.m
+++ utility/load_index_file.m
@@ -1,4 +1,4 @@
-function index_table = load_index_file(file_path)
+function index_table = load_index_file(file_path,base_directory)
 
 opts=delimitedTextImportOptions('Whitespace',sprintf('\t '),...
     'Delimiter', {sprintf('\t'),' '}, ...
@@ -12,4 +12,13 @@ index_table = readtable(file_path,opts);
 idx_test=0:size(index_table,1)-1;
 if nnz(index_table.index(:)~=idx_test(:))
     error('index file error, non-contiguous');
+end
+
+
+if exist('base_directory','var') && ~path_is_absolute(index_table.fid{1})
+    % local_index.fid = cellfun(@(c) fullfile(scan_data_setup.main,c), local_index.fid)
+    idx_ready = cellfun(@(c) ~isempty(c),index_table.fid);
+    full_paths = cellfun(@(c) fullfile(base_directory,c), index_table.fid, 'UniformOutput', false);
+    full_paths =cellfun(@(c) path_convert_platform(c,'lin'), full_paths ,'UniformOutput',false);
+    index_table.fid(idx_ready) = full_paths(idx_ready);
 end
